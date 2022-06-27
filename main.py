f350d722852c9bf007ca61fa9d7cf85519956beb
591988016@qq.com <591988016@qq.com>
2022-06-27 17:59:12 +0800

    测试代码

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-ces
+import sys,math
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import qdarkstyle
+
+class Drawing(QWidget):
+    def __init__(self,parent=None):
+        super(Drawing,self).__init__(parent)
+        self.resize(300,200)
+        self.setFixedSize(self.width(), self.height())
+        self.setWindowTitle('在窗口画点')
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        btn = QPushButton('关闭', self)
+        btn.clicked.connect(QCoreApplication.instance().quit)
+
+
+
+
+    def paintEvent(self,event):
+        #初始化绘图工具
+        qp=QPainter()
+        #开始在窗口绘制
+        qp.begin(self)
+        #自定义画点方法
+        self.drawPoints(qp)
+        #结束在窗口的绘制
+        qp.end()
+
+    def drawPoints(self,qp):
+        qp.setPen(Qt.green)
+        size=self.size()
+
+        for i in range(1000):
+            #绘制郑玄函数图像，它的周期是【-100,100】
+            x=100*(-1+2.0*i/1000)+size.width()/2.0
+            y=-50*math.sin((x-size.width()/2.0)*math.pi/50)+size.height()/2.0
+
+            qp.drawPoint(x,y)
+
+if __name__ == '__main__':
+    app=QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    demo=Drawing()
+    demo.show()
+    sys.exit(app.exec_())
